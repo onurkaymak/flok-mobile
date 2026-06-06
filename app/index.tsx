f1814b826a851fcfb4082df2285ebb42d8b7bd44
Onur Kaymak <onurkaymak@outlook.com>
2026-06-06 14:50:21 -0700

    Add Uniwind packages.

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import "@/global.css";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-blue-500">Test</Text>
+    </View>
+  );
+}
